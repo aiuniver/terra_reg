cda70c9514c474579ab83a5089434039f843a886
BL146U <bl146u@gmail.com>
2021-04-12 17:21:12 +0300

    Domain prefix only ascii_lowercase

--- utils/registartion_utils.py
+++ utils/registartion_utils.py
@@ -6,18 +6,22 @@ from django.core.mail import send_mail
 
 
 def generate_prefix(length):
-    char_range = string.ascii_letters + string.digits
-    prefix = ''.join(random.sample(char_range, length))
+    char_range = string.ascii_lowercase + string.digits
+    prefix = "".join(random.sample(char_range, length))
     return prefix
 
 
 def send_registration_mail(**kwargs):
-    title = 'Регистрационные данные TerraAI'
-    user = kwargs['user']
-    message = f'Ваши данные для регистрации:\n' \
-              f'Токен: {kwargs["token"]}\n' \
-              f'Доменный префикс: {kwargs["prefix"]}\n' \
-              f'Серверный порт: {kwargs["port"]}' \
-              f'Jupiter ноутбук для логина и установки: ...'
+    title = "Регистрационные данные TerraAI"
+    user = kwargs["user"]
+    message = (
+        f"Ваши данные для регистрации:\n"
+        f'Токен: {kwargs["token"]}\n'
+        f'Доменный префикс: {kwargs["prefix"]}\n'
+        f'Серверный порт: {kwargs["port"]}'
+        f"Jupiter ноутбук для логина и установки: ..."
+    )
 
-    return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+    return send_mail(
+        title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False
+    )
